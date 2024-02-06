c24e2f66b9183294deee904c21ad1874197df200
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-02-06 13:40:05 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Função em que a maquina escolhe de forma aleatória
 function choice_comp(){
     var choice_number = Math.floor(Math.random()*3);
     if (choice_number == 0){
@@ -10,15 +11,33 @@ function choice_comp(){
         return 'tesoura';
     }
 }
+
+//Função que conta o score de vitórias do usuário
 function countscore() {
-    var scorehtml = document.querySelector('.points');
-    var scorepoints = Number.parseInt(scorehtml)
-    scorehtml.innerText = scorepoints+1;
-    
+    var scoreElement = document.querySelector('.points');
+    var scorepoints = Number.parseInt(scoreElement.innerText);
+    scorepoints++;
+    if (scorepoints<10) {
+        scoreElement.innerText = '0'+scorepoints;
+    }
+    else{
+        scoreElement.innerText = scorepoints;
+    }
 }
+
+//Função que zera o score
+function zerascore() {
+    var scoreElement = document.querySelector('.points');
+    scoreElement.innerText = '00';
+}
+
+
+//Função que compara a jogada do jogador e da máquina
 function play(user_btn){
-    const choice_pc = choice_comp()
-    alert('PC ESCOLHEU '+ choice_pc);
+    //const choice_pc = choice_comp()
+    const choice_pc = 'pedra';
+
+    //alert('PC ESCOLHEU '+ choice_pc);
     if (user_btn === 'papel' && choice_pc === 'papel') {
          alert('Empate');   
     }
@@ -29,7 +48,8 @@ function play(user_btn){
         alert('Empate');   
     }
     else if (user_btn === 'papel' && choice_pc === 'tesoura') {
-        alert('PC ganhou');   
+        alert('PC ganhou');  
+        zerascore(); 
     }
     else if (user_btn === 'papel' && choice_pc === 'pedra') {
         alert('VOCÊ ganhou');
@@ -40,10 +60,12 @@ function play(user_btn){
         countscore();   
     }
     else if (user_btn === 'tesoura' && choice_pc === 'pedra') {
-        alert('PC ganhou');   
+        alert('PC ganhou');
+        zerascore();   
     }
     else if (user_btn === 'pedra' && choice_pc === 'papel') {
-        alert('PC ganhou');   
+        alert('PC ganhou');
+        zerascore();   
     }
     else if (user_btn === 'pedra' && choice_pc === 'tesoura') {
         alert('VOCÊ ganhou');
@@ -51,6 +73,7 @@ function play(user_btn){
     }
 }
 
+// Função que mostra o alerta de REGRAS
 function showhide(){
     Swal.fire({
         title: "REGRAS",
