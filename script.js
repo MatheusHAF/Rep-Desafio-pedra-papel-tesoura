cc661e78f3e5c04d118c4ddb414404ce4cd1a8bb
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-02-23 17:19:00 -0300

    .

--- script.js
+++ script.js
@@ -89,11 +89,76 @@ function mudamainescolha(user,pc){
     //desaparecer a primeira div
     document.getElementById('cont-1').style.display = 'none';
     
-    //alterar button das divs
+    //alterar button e imgs das divs
+    var botao_player = document.getElementById("pl-ch");
+    var botao_house = document.getElementById("hs-ch");
 
+    var img_player = document.getElementById("img-pl");
+    var img_house = document.getElementById("img-hs");
+
+    //arrumar a proxima div
+    var btnplayer = document.getElementById("pk-pl-btn");
+    var btnhouse = document.getElementById("pk-hs-btn");
+
+    var imgplayer = document.getElementById("pk-pl-img");
+    var imghouse = document.getElementById("pk-hs-img");
+
+    if (user == "papel") {
+        botao_player.id = "pap-btn";
+        img_player.src = "./rock-paper-scissors-master/images/icon-paper.svg";
+        //
+        btnplayer.id = "pap-btn";
+        imgplayer.src = "./rock-paper-scissors-master/images/icon-paper.svg";
+    }
+    else if (user == "tesoura") {
+        botao_player.id = "tes-btn";
+        img_player.src = "./rock-paper-scissors-master/images/icon-scissors.svg";
+        //
+        btnplayer.id = "tes-btn";
+        imgplayer.src = "./rock-paper-scissors-master/images/icon-scissors.svg";
+    }
+    else if (user == "pedra") {
+        botao_player.id = "ped-btn";
+        img_player.src ="./rock-paper-scissors-master/images/icon-rock.svg";
+        //
+        btnplayer.id = "ped-btn";
+        imgplayer.src = "./rock-paper-scissors-master/images/icon-rock.svg";
+    }
+    if (pc == "papel") {
+        botao_house.id ="pap-btn";
+        img_house.src = "./rock-paper-scissors-master/images/icon-paper.svg";
+        //
+        btnhouse.id = "pap-btn";
+        imghouse.src = "./rock-paper-scissors-master/images/icon-paper.svg";
+    }
+    else if(pc == "tesoura"){
+        botao_house.id ="tes-btn";
+        img_house.src = "./rock-paper-scissors-master/images/icon-scissors.svg";
+        //
+        btnhouse.id = "tes-btn";
+        imghouse.src = "./rock-paper-scissors-master/images/icon-scissors.svg";
+    }
+    else if(pc == "pedra"){
+        botao_house.id ="ped-btn";
+        img_house.src = "./rock-paper-scissors-master/images/icon-rock.svg";
+        //
+        btnhouse.id = "ped-btn";
+        imghouse.src = "./rock-paper-scissors-master/images/icon-rock.svg";
+    }
+    
     //mostrar div
     document.getElementById('cont-2').style.display = 'flex';
-    
+
     //timezinho de uns 3s e mostrar a proxima div
     //document.getElementById('cont-3').style.display = 'block';
+    setTimeout(function() {
+        // Coloque aqui as funções que deseja executar após o atraso de 3 segundos
+        document.getElementById('cont-2').style.display = 'none';
+        document.getElementById('cont-3').style.display = 'flex';
+      }, 1500); // 3000 milissegundos = 3 segundos
+}
+
+function playagain() {
+    document.getElementById('cont-3').style.display = 'none';
+    document.getElementById('cont-1').style.display = 'block';
 }
